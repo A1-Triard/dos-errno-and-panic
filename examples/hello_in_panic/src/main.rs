11122ce9fb6ef5f9c1abc9de9e6f687a3e530ec0
Warlock <internalmike@gmail.com>
2022-12-30 18:21:46 +0300

    hinpan

--- examples/hello_in_panic/src/main.rs
+++ examples/hello_in_panic/src/main.rs
@@ -6,6 +6,20 @@
 
 extern crate dos_errno_and_panic;
 extern crate pc_atomics;
+extern crate rlibc;
+
+mod no_std {
+    #[no_mangle]
+    extern "C" fn _aulldiv() -> ! { panic!("10") }
+    #[no_mangle]
+    extern "C" fn _aullrem() -> ! { panic!("11") }
+    #[no_mangle]
+    extern "C" fn _chkstk() { }
+    #[no_mangle]
+    extern "C" fn _fltused() -> ! { panic!("13") }
+    #[no_mangle]
+    extern "C" fn strlen() -> ! { panic!("14") }
+}
 
 extern {
     type PEB;
@@ -14,5 +28,5 @@ extern {
 #[allow(non_snake_case)]
 #[no_mangle]
 extern "stdcall" fn mainCRTStartup(_: *const PEB) -> u64 {
-    0
+    panic!("Hello! I am panicking.");
 }
